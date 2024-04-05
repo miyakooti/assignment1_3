8abb9b9cb733720dfc53a2f5d74dc8946fbd3252
Ubuntu <ubuntu@ip-172-31-87-157.ec2.internal>
2024-04-05 04:00:05 +0000

    add git ignore

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ import boto3
 import os
 import logging
 
+from aws_credentials import get_session
+
 
 
 app = Flask(__name__)
@@ -13,12 +15,7 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = os.urandom(24)
 
 # AWS認証情報の設定
-session = boto3.Session(
-    aws_access_key_id='ASIAQ3EGVH4JRBUGXUCY',
-    aws_secret_access_key='ks2cwzMttFQzG2mLCZLWH6kyIzsQCQTkXDweSMnI',
-    aws_session_token='IQoJb3JpZ2luX2VjEHMaCXVzLXdlc3QtMiJGMEQCIFenjpnD4AHcmQ3YQI9SCo7wIPhJbF5LgrGDNTRrhkHRAiBYTaJWA5ZxT3UUlkH+6a6Wd7lrefwwZ65WUAsfEOKzZCq/Agic//////////8BEAAaDDA1ODI2NDQ2OTI2NyIM9hk+VOGkCBQHCHyVKpMCXjnDx9M8pin7LoIiq9GqCKxhDEeP7rLdosGvnSrJh1ZVLQCyGhiYqWNWs5xFkcty7sMXrxoyYT6jrEtfF80Q/WghoTdz0SZWbMKvnSiWcNjfOPaq+49rBOr56RhbUtkgigj+/7Pho6H2PJNpcrqgXmmOjm0Z4pn1IFFsxt7n3BKvNNrJxVxSSu7D7Jb404y+U42ZSuLp6pNFP0z9TjOEtJPpVci9crugb0gt4hYG8Sf/VmXWBTLHZOKC2ncQXYapZRbhTzfsoN3lBX9H3EHuYjMG06Z9c4BEBcQ06PqNoBXFY46r/f3jFklocXtoXY3BkIJZil+mvlf9WaMY7hDWO+dJv1i0ApQrPN5+/22idPIO5dowv829sAY6ngHY4WdMI5k28A4yWwy4fWBM+1ikhdMLyPFMvxG4bd0GstbDyMixh6QUziT+TaC6VAPLvMCyHy/Oxoi5kk+5n7wUiVIJcZ5VmP+kgLqQVq5VPweiaEVwtFaJPoysUfLo3LFYzl+8+rq0Y2kGx7T0lGzPWVd36FKgdumjEpcFv8ZEh3+NU5sGdQYuJ/sGCSdNxe7o+qoKwtYGIS1/9W4N8g==',  # オプション（一般的には必要ありません）
-    region_name='us-east-1'
-)
+session = get_session()
 
 # AWSクライアントの作成
 s3_client = session.client('s3')
